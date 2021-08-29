d05a4c2eb42e545bf51de4f95ce1ac4baf4fc3bc
Tran Le <tle@topcon.com>
2021-08-29 23:57:03 +0300

    update calculator app with new styles and more functions

--- script.js
+++ script.js
@@ -1,6 +1,23 @@
-function compute()
-{
-    p = document.getElementById("principal").value;
-    
+function updateTextInput(val) {
+    document.getElementById("textInput").innerHTML = val+'%';
+    document.getElementById('textInput').value = val;
+
 }
-        
+
+function compute() {
+    const amount = document.getElementById("amount").value;
+    const rate = document.getElementById("textInput").value;
+    const years = document.getElementById("years").value;
+
+    if( parseInt(amount) <= 0 ){
+        window.alert('please enter a positive number');
+        return;
+    }
+
+    const total = (parseInt(amount) * parseInt(rate) * parseInt(years) / 100)
+    document.getElementById("amount-output").innerHTML = total;
+    document.getElementById("rate-output").innerHTML = total;
+    document.getElementById("year-output").innerHTML = total;
+    document.getElementById("result").innerHTML = total;
+
+}
